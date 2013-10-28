7f77e8747e7b7cb45aa2a871c71c448ee8ca1c00
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-10-28 17:10:00 +0800

    First

---
+++ curl.sh
@@ -0,0 +1,4 @@
+curl -F 'access_token=CAACEdEose0cBAOzrcyPnZBSO6ajPCSbnwoTbX1ZB3FvDIgNVZAwY4ou0hScCdxJQXC0Ps4x2rrZA1MvJBocg65Eeur9qO1YtL7V8IpVQ7IaDD6akC0L728LH9ktmPYcq1RZBUSfNmtjIIPowwzr3l3Q885ugB9exRwNN9UQSZArFMQIEgN4xV2oZBaEZC1WANsGuwhv7PdjZB3gZDZD' \ 
+     -F 'source=@/home/neilmarion/DEV/imagemagick_exp/imgp8233_crop.jpg' \
+     -F 'message=Test' 
+     'https://graph.facebook.com/me/photos'
